889148f8decf3ae394ca18dc85dc0cfcc321e44e
daniel <dkinzler@wikimedia.org>
2026-05-08 16:45:33 +0200

    Record relative path

--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -26,24 +26,31 @@ func (s *Scanner) Scan(ctx context.Context, dir string, out chan<- *recorder.Rec
 		if err != nil {
 			return err
 		}
+
 		if !s.IncludeDotFiles && len(d.Name()) > 1 && d.Name()[0] == '.' {
 			if d.IsDir() {
 				return fs.SkipDir
 			}
 			return nil
 		}
+
 		if d.IsDir() {
 			if !s.Recursive && path != dir {
 				return fs.SkipDir
 			}
 			return nil
 		}
+
 		info, err := d.Info()
 		if err != nil {
 			return err
 		}
+		relPath, err := filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
 
-		rec := &recorder.Record{path, info}
+		rec := &recorder.Record{relPath, info}
 
 		select {
 		case out <- rec:
