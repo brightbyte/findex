56b2982603dc6f090ed7a24fa368591f5d424f06
daniel <dkinzler@wikimedia.org>
2026-05-08 20:35:07 +0200

    Add list command

--- cmd/findex/main.go
+++ cmd/findex/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"findex/pkg/list"
 	"findex/pkg/recorder"
 	"findex/pkg/scanner"
 	"flag"
@@ -14,39 +15,90 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-var includeDotFiles bool
-var noRecursion bool
-var dry bool
 var quiet bool
-var batchSize int
-var dir string
 
 func init() {
 	if runtime.GOOS == "windows" {
 		fmt.Fprintln(os.Stderr, "warning: hard link detection is not supported on Windows")
 	}
 
-	flag.BoolVar(&includeDotFiles, "a", false, "include dot files and directories")
-	flag.BoolVar(&includeDotFiles, "all", false, "include dot files and directories")
-
-	flag.BoolVar(&noRecursion, "no-recursion", false, "do not recurse into subdirectories")
-	flag.BoolVar(&noRecursion, "R", false, "do not recurse into subdirectories")
-
-	flag.BoolVar(&dry, "dry", false, "print results to stdout instead of writing to database")
 	flag.BoolVar(&quiet, "q", false, "suppress progress output")
 	flag.BoolVar(&quiet, "quiet", false, "suppress progress output")
-	flag.IntVar(&batchSize, "batch-size", 100, "number of inserts per transaction")
 	flag.Parse()
-
-	if flag.NArg() < 1 {
-		fmt.Fprintln(os.Stderr, "usage: findex [-a] [-R] [-dry] [-batch-size N] <directory>")
-		os.Exit(1)
-	}
-
-	dir = flag.Arg(0)
 }
 
 func main() {
+	if flag.NArg() < 1 {
+		cmdHelp()
+		os.Exit(1)
+	}
+
+	cmd := flag.Arg(0)
+	args := flag.Args()[1:]
+
+	switch cmd {
+	case "help":
+		cmdHelp()
+	case "update":
+		cmdUpdate(args)
+	case "list":
+		cmdList(args)
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmd)
+		cmdHelp()
+		os.Exit(1)
+	}
+}
+
+func cmdHelp() {
+	fmt.Println(`usage: findex [-q] <command> [options] <directory>
+
+Global flags:
+  -q, -quiet    suppress progress output
+
+Commands:
+  update        scan a directory and record files to .findex.sqlite
+  list          list contents of an existing .findex.sqlite
+  help          show this help
+
+update usage:
+  findex update [options] <directory>
+
+  Options:
+  -a, -all          include dot files and directories
+  -R, -no-recursion do not recurse into subdirectories
+  -dry              print results to stdout instead of writing to database
+  -batch-size N     number of inserts per transaction (default 100)
+
+list usage:
+  findex list [options] <directory> [prefix]
+
+  Options:
+  -g, -glob         filter output by glob pattern`)
+}
+
+func cmdUpdate(args []string) {
+	fs := flag.NewFlagSet("update", flag.ExitOnError)
+	var includeDotFiles bool
+	var noRecursion bool
+	var dry bool
+	var batchSize int
+	fs.BoolVar(&includeDotFiles, "a", false, "include dot files and directories")
+	fs.BoolVar(&includeDotFiles, "all", false, "include dot files and directories")
+	fs.BoolVar(&noRecursion, "no-recursion", false, "do not recurse into subdirectories")
+	fs.BoolVar(&noRecursion, "R", false, "do not recurse into subdirectories")
+	fs.BoolVar(&dry, "dry", false, "print results to stdout instead of writing to database")
+	fs.BoolVar(&quiet, "q", false, "suppress progress output")
+	fs.BoolVar(&quiet, "quiet", false, "suppress progress output")
+	fs.IntVar(&batchSize, "batch-size", 100, "number of inserts per transaction")
+	fs.Parse(args)
+
+	if fs.NArg() < 1 {
+		fmt.Fprintln(os.Stderr, "usage: findex update [-a] [-R] [-dry] [-batch-size N] <directory>")
+		os.Exit(1)
+	}
+	dir := fs.Arg(0)
+
 	var rec recorder.Recorder
 	if dry {
 		rec = &recorder.Echo{}
@@ -68,14 +120,36 @@ func main() {
 	channel := make(chan *recorder.Record)
 	errs.Go(func() error {
 		defer close(channel)
-		err := s.Scan(ctx, dir, channel)
-		return err
+		return s.Scan(ctx, dir, channel)
 	})
 	errs.Go(func() error {
 		return recorder.Consume(dir, rec, channel)
 	})
 
 	if err := errs.Wait(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
+
+func cmdList(args []string) {
+	fs := flag.NewFlagSet("list", flag.ExitOnError)
+	var glob string
+	fs.StringVar(&glob, "g", "", "filter by glob pattern on basename")
+	fs.StringVar(&glob, "glob", "", "filter by glob pattern on basename")
+	fs.Parse(args)
+
+	if fs.NArg() < 1 {
+		fmt.Fprintln(os.Stderr, "usage: findex list [-g pattern] <directory> [prefix]")
+		os.Exit(1)
+	}
+	dir := fs.Arg(0)
+	prefix := ""
+	if fs.NArg() >= 2 {
+		prefix = fs.Arg(1)
+	}
+
+	if err := list.List(dir, prefix, glob); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
 	}
